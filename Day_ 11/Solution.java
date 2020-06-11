268acba270c6785a4e6140b8492c5ba801c2cd41
Krish <vamsikrish.49@gmail.com>
2020-06-11 13:01:41 +0530

    Day 11 Solution

---
+++ Day_ 11/Solution.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/sort-colors/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        if(nums.length == 0 || nums.length == 1)
+            return;
+        
+        int start = 0;
+        int end = nums.length - 1;
+        int index = 0;
+        
+        while(index <= end) {
+            if(nums[index] == 0) {
+                nums[index] = nums[start];
+                nums[start] = 0;
+                start++;
+                index++;
+            }
+            else if(nums[index] == 2) {
+                nums[index] = nums[end];
+                nums[end] = 2;
+                end--;
+            }
+            else {
+                index++;
+            }
+        }
+    }
+}
