58d292de4c604af6f7f7930e7ea386cfd223d65a
Krish <vamsikrish.49@gmail.com>
2020-06-14 23:44:50 +0530

    Day 14 Solution

--- Day_ 14/Solution.java
+++ Day_ 14/Solution.java
@@ -1,1 +1,35 @@
 
+public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+    {
+        Map<Integer,List<int[]>> map=new HashMap<>();
+        for(int[] i:flights)
+        {
+            map.putIfAbsent(i[0],new ArrayList<>());
+            map.get(i[0]).add(new int[]{i[1],i[2]});
+        }
+        int step=0;
+        Queue<int[]> q=new LinkedList<>();
+        q.offer(new int[]{src,0});
+        int ans=Integer.MAX_VALUE;
+        while(!q.isEmpty())
+        {
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                int[] curr=q.poll();
+                if(curr[0]==dst)
+                    ans=Math.min(ans,curr[1]);
+                if(!map.containsKey(curr[0]))
+                    continue;
+                for(int[] f:map.get(curr[0]))
+                {
+                    if(curr[1]+f[1]>ans)      //Pruning
+                        continue;
+                    q.offer(new int[]{f[0],curr[1]+f[1]});
+                }
+            }
+            if(step++>K)
+                break;
+        }
+        return ans==Integer.MAX_VALUE?-1:ans;
+    }
