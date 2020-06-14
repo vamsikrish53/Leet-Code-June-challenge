64840636c8fecff15ebdf4a15ad9a537fa8ceb2f
Krish <vamsikrish.49@gmail.com>
2020-06-14 23:53:13 +0530

    Day 14 Solution

--- Day_ 14/Solution.java
+++ Day_ 14/Solution.java
@@ -1,35 +1,27 @@
+// https://leetcode.com/problems/cheapest-flights-within-k-stops/
 
-public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
-    {
-        Map<Integer,List<int[]>> map=new HashMap<>();
-        for(int[] i:flights)
-        {
-            map.putIfAbsent(i[0],new ArrayList<>());
-            map.get(i[0]).add(new int[]{i[1],i[2]});
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, Map<Integer, Integer>> prices = new HashMap<>();
+        for (int[] f : flights) {
+            if (!prices.containsKey(f[0])) prices.put(f[0], new HashMap<>());
+            prices.get(f[0]).put(f[1], f[2]);
         }
-        int step=0;
-        Queue<int[]> q=new LinkedList<>();
-        q.offer(new int[]{src,0});
-        int ans=Integer.MAX_VALUE;
-        while(!q.isEmpty())
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                int[] curr=q.poll();
-                if(curr[0]==dst)
-                    ans=Math.min(ans,curr[1]);
-                if(!map.containsKey(curr[0]))
-                    continue;
-                for(int[] f:map.get(curr[0]))
-                {
-                    if(curr[1]+f[1]>ans)      //Pruning
-                        continue;
-                    q.offer(new int[]{f[0],curr[1]+f[1]});
+        Queue<int[]> pq = new PriorityQueue<>((a, b) -> (Integer.compare(a[0], b[0])));
+        pq.add(new int[] {0, src, k + 1});
+        while (!pq.isEmpty()) {
+            int[] top = pq.remove();
+            int price = top[0];
+            int city = top[1];
+            int stops = top[2];
+            if (city == dst) return price;
+            if (stops > 0) {
+                Map<Integer, Integer> adj = prices.getOrDefault(city, new HashMap<>());
+                for (int a : adj.keySet()) {
+                    pq.add(new int[] {price + adj.get(a), a, stops - 1});
                 }
             }
-            if(step++>K)
-                break;
         }
-        return ans==Integer.MAX_VALUE?-1:ans;
+        return -1;
     }
+}
