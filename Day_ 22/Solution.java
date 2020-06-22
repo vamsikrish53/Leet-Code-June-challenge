ed6598bcdeb9c2f17d9accdb31c588f24b560465
Krish <vamsikrish.49@gmail.com>
2020-06-22 21:53:58 +0530

    Day 22 Solution

---
+++ Day_ 22/Solution.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/single-number-ii/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+    int res = 0;
+    for(int i = 0; i < 32; i++){
+        int sum = 0;
+        for(int n: nums)
+            if((n >> i & 1) == 1)
+                sum++;
+        sum %= 3;
+        res |= sum<<i;
+    }
+    return res;
+    }
+}
